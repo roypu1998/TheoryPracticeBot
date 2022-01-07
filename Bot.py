d4ced5461b13af100ac7a8760905e95d64b45713
roy <57131717+roypu1998@users.noreply.github.com>
2022-01-07 22:12:11 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,13 +7,14 @@ import os
 
 token = os.environ.get('API_TOKEN',None)
 bot = TeleBot(token)
+global phone_no, correct, incorrect_Point, correct_Point, markup, limit, mid, msgIncorrect_id
 
-global phone_no, correct, incorrect_Point, correct_Point, markup, limit
-
+msgIncorrect_id=[]
 incorrect_Point = 0
 correct_Point = 0
 limit = 0
 correct=''
+mid=0
 
 markup =MarkUp([])
 
@@ -29,7 +30,7 @@ def return_correct_answer():
     return correct
 
 def sendQuestion(call):
-    global correct, markup
+    global correct, markup, mid
     randomQ = randint(1,1274)
     theoryQ = QuestionTheory(randomQ)
     question, all_answers, correct_answer, img = theoryQ.get_question()
@@ -37,9 +38,10 @@ def sendQuestion(call):
     correct = correct_answer
     markup = MarkUp(all_answers)
     if img!="No":
-        bot.send_photo(chat_id=call.chat.id,photo=img,caption='שאלה :\n'+question,reply_markup=markup.get_answers_Reoplybtn())
+        mid = bot.send_photo(chat_id=call.chat.id,photo=img,caption='שאלה :\n'+question,reply_markup=markup.get_answers_Reoplybtn())
     else:
-        bot.send_message(chat_id=call.chat.id,text='שאלה :\n'+question,reply_markup =markup.get_answers_Reoplybtn())
+        mid = bot.send_message(chat_id=call.chat.id,text='שאלה :\n'+question,reply_markup =markup.get_answers_Reoplybtn())
+
 
 
 
@@ -54,9 +56,10 @@ def start_game(message):
 
 
 
+
 @bot.message_handler(func=lambda call: True)
 def answer_message_query(call):
-    global incorrect_Point, correct_Point, limit
+    global incorrect_Point, correct_Point, limit, mid, msgIncorrect_id
 
     if call.text == "Ok, lets start" :
         sendQuestion(call)
@@ -65,24 +68,29 @@ def answer_message_query(call):
         sendQuestion(call)
 
     elif call.text == "/Finish":
-        bot.send_message(call.chat.id,f"Ok! You get {correct_Point} Correct Answers and {incorrect_Point} Mistakes.")
-        limit = 0
-        correct_Point, incorrect_Point = 0, 0
         bot.send_message(call.chat.id,f"GoodBye {call.from_user.first_name} 👋 ")
 
-    elif limit > 29 :
+    elif limit > 29:
         bot.send_message(call.chat.id,f"You pass {limit - 1} Question.\n"
                                       f"Do you want continue? \n"
                                       f"/continue or /Finish")
 
     elif call.text == return_correct_answer():
+
         correct_Point += 1
         bot.send_message(call.chat.id,f"EXCELLENT ! You have {correct_Point} Correct Answers. ")
+        bot.delete_message(call.chat.id, mid.message_id)
+        bot.delete_message(call.chat.id, call.message_id)
+        if msgIncorrect_id != []:
+            for id in msgIncorrect_id:
+                bot.delete_message(call.chat.id,id)
+                msgIncorrect_id.remove(id)
         sendQuestion(call)
         limit += 1
 
     else:
         incorrect_Point += 1
         bot.send_message(call.chat.id,f'You have {incorrect_Point} Mistakes, try again.')
+        msgIncorrect_id.append(call.message_id)
 
 bot.polling()
