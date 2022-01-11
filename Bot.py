c4ad48effae0552498315e8ddaf68c1410a88c26
roy <57131717+roypu1998@users.noreply.github.com>
2022-01-11 02:14:09 +0200

    Update Bot.py
    add user database

--- Bot.py
+++ Bot.py
@@ -3,12 +3,15 @@ from telebot.types import ReplyKeyboardRemove
 from db_TheoryQuestion import QuestionTheory
 from MarkUp import MarkUp
 from random import randint
+from DB_FireBase import User
 import os
 
 
 token = os.environ.get('API_TOKEN',None)
-bot = TeleBot(token)
-global phone_no, correct, incorrect_Point, correct_Point, markup, limit, mid, msgIncorrect_id
+bot = TeleBot(token=token)
+
+
+global phone_no, correct, incorrect_Point, correct_Point, markup, limit, mid, msgIncorrect_id, app, flagStart
 
 msgIncorrect_id=[]
 incorrect_Point = 0
@@ -16,15 +19,40 @@ correct_Point = 0
 limit = 0
 correct=''
 mid=0
+flagStart = False
 
-markup =MarkUp([])
+markup = MarkUp([])
+@bot.message_handler(func=lambda call: True if call.text.isdigit() and len(call.text) > 8 and len(call.text) < 12 else False)
+def postOnDB(call):
+    if call.text == str(call.from_user.id) and flagStart :
+        app.PutOnDatabase()
+        msg=bot.send_message(call.chat.id, f'Post {call.from_user.username} on Database ...')
+        bot.edit_message_text('Ok, you are in!\nEnter /start to check it is works',call.chat.id, msg.message_id)
+    else:
+        bot.send_message(call.chat.id, f'[WARNING] id does not exists.\ntry to register again /start')
+
+
+@bot.message_handler(func=lambda call: True if call.text.isdigit() and len(call.text) > 3 and len(call.text) < 10 else False)
+def number_handler(msg):
+    bot.send_message(msg.chat.id,f'[WARNING] : wrong input.')
+
 
 @bot.message_handler(commands='start')
 def handler_on_start(message):
+    global app,flagStart
+    flagStart = True
     bot.send_chat_action(message.chat.id,'typing')
-    bot.send_message(message.chat.id,f'Hello {message.from_user.first_name}!\n'
+    username = message.from_user.username
+    user_id = message.from_user.id
+    app = User(user_name=username, user_id=user_id)
+    flag = app.GetFromDatabase(user_name=username)
+    if flag :
+        bot.send_message(message.chat.id,f'Hello {message.from_user.first_name}!\n'
                                      f'Welcome to Theory practice.\n'
                                      f'For start please press /StartGame .')
+    else:
+        bot.send_message(message.chat.id, f'Please go to https://t.me/userinfobot ,get your id and write it.')
+        bot.register_next_step_handler(message,postOnDB)
 
 def return_correct_answer():
     global correct
@@ -58,7 +86,7 @@ def start_game(message):
 
 
 
-@bot.message_handler(func=lambda call: True)
+@bot.message_handler(func=lambda call: True )
 def answer_message_query(call):
     global incorrect_Point, correct_Point, limit, mid, msgIncorrect_id
 
